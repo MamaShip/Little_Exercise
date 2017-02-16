73b0889caf55896eb32ec3edef4178ab526c69a8
MamaShip <youdangls@gmail.com>
2017-02-16 12:12:12 +0800

    修改五星的绘制方式

--- TurtleDrawFlag/TurtleDrawFlag.py
+++ TurtleDrawFlag/TurtleDrawFlag.py
@@ -4,7 +4,9 @@ def drawStar(x,y):
     turtle.goto(x,y)
     turtle.pendown()
     for i in range(5):
-        turtle.forward(10)
+        turtle.forward(4)
+        turtle.left(72)
+        turtle.forward(4)
         turtle.right(144)
     turtle.penup()
 def drawRectangle(x,y,chang,kuan):
@@ -25,25 +27,26 @@ turtle.pencolor('red')
 for i in range(0,13,2):
     turtle.fillcolor('red')
     turtle.fill(True)
-    drawRectangle(0,-(i*14),14,340)
+    drawRectangle(0,-(i*20),20,440)
     turtle.fill(False)
 
 turtle.pencolor('blue')
 turtle.fillcolor('blue')
 turtle.fill(True)
-drawRectangle(0,0,98,170)
+drawRectangle(0,0,140,220)
 turtle.fill(False)
+
 turtle.pencolor('white')
-for j in range(10,100,30):
-    for i in range(5,170,25):
+for j in range(8,140,30):
+    for i in range(15,220,35):
         print i,j
         turtle.fillcolor('white')
         turtle.fill(True)
         drawStar(i,-j)
         turtle.fill(False)
 
-for j in range(25,100,30):
-    for i in range(18,160,25):
+for j in range(23,140,30):
+    for i in range(32,200,35):
         print i,j
         turtle.fillcolor('white')
         turtle.fill(True)
